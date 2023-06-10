cfc9855c9b45b69f28a1334750e020b1a6b76e17
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-06-10 13:08:05 +0700

    add sign in and user data model

---
+++ app/src/main/java/com/exfarnanda1945/composegooglesignin/data/model/SignInResult.kt
@@ -0,0 +1,6 @@
+package com.exfarnanda1945.composegooglesignin.data.model
+
+data class SignInResult(
+    val data: UserData?,
+    val errorMessage: String?
+)
---
+++ app/src/main/java/com/exfarnanda1945/composegooglesignin/data/model/UserData.kt
@@ -0,0 +1,7 @@
+package com.exfarnanda1945.composegooglesignin.data.model
+
+data class UserData(
+    val userId: String,
+    val username: String?,
+    val profilePictureUrl: String?
+)
