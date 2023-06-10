8e6e42c6dd1631a127eb6fbf3f80d90317f6138e
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-06-10 13:12:12 +0700

    implement navigation

--- app/src/main/java/com/exfarnanda1945/composegooglesignin/MainActivity.kt
+++ app/src/main/java/com/exfarnanda1945/composegooglesignin/MainActivity.kt
@@ -6,12 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.exfarnanda1945.composegooglesignin.ui.theme.ComposeGoogleSignInTheme
+import com.exfarnanda1945.composegooglesignin.utils.SetupNavGraph
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +23,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navHostController = rememberNavController()
+                    SetupNavGraph(
+                        navHostController = navHostController,
+                        context = applicationContext
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeGoogleSignInTheme {
-        Greeting("Android")
     }
 }
